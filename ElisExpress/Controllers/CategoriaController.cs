f2ec1fbdd65cb12baddd9b3a3bef4ef54396ec4a
erocham <erocham@ucenfotec.ac.cr>
2020-12-19 09:33:51 -0600

    Vistas crear productos, categorias, estilos, tablas, index.

---
+++ ElisExpress/Controllers/CategoriaController.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ElisExpress.Models;
+using ElisExpress.Repositories;
+using ElisExpress.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace ElisExpress.Controllers
+{
+    public class CategoriaController : Controller
+    {
+        private RepositorioCategorias repositorioCategorias;
+
+        public CategoriaController(RepositorioCategorias repositorioCategorias)
+        {
+            this.repositorioCategorias = repositorioCategorias;
+        }
+
+        public IActionResult Categorias()
+        {
+
+            var modelo = new CategoriaViewModel();
+
+            modelo.Categorias = repositorioCategorias.Categorias.ToList();
+
+            return View(modelo);
+        }
+
+        [HttpGet]
+        public IActionResult CrearCategoria()
+        {
+            return View();
+        }
+
+
+        [HttpPost]
+        public IActionResult CrearCategoria(Categoria categoria)
+        {
+            repositorioCategorias.Categorias.Add(categoria); //Ir a la base de datos a guardarlo 
+            return RedirectToAction("Categorias");
+        }
+    }
+}
+

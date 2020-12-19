b36e4915cc598b21cf7887f83fd965b2dfa524e0
erocham <erocham@ucenfotec.ac.cr>
2020-12-19 00:03:38 -0600

    Elian Rocha - Nuevas vistas, estilos, cards, productos, categorias, inicio del carrito, index

---
+++ ElisExpress/ViewModels/CarritoViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ElisExpress.Models;
+
+namespace ElisExpressWebUI.ViewModels
+{
+    public class CarritoViewModel
+    {
+        public List<Carrito> Carritos { get; set; }
+    }
+}
